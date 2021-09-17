63ffa5813a2331675956e131179c030cfdfbabbd
Jeevesh8 <creativityinczenyoga@gmail.com>
2021-09-17 13:06:57 +0530

    Prompt relation prediction

--- exp_scripts/relation_type/prompt_5_cons_ft_runs_CmvModes.py
+++ exp_scripts/relation_type/prompt_5_cons_ft_runs_CmvModes.py
@@ -1,4 +1,4 @@
-import warnings
+import warnings, random
 from typing import Tuple, List, Union
 warnings.filterwarnings('ignore')
 
@@ -15,6 +15,11 @@ from arg_mining.datasets.cmv_modes import load_dataset, data_config
 ac_dict = data_config["arg_components"]
 rel_type_dict = {rel : i for i, rel in enumerate(data_config["adv_relations_map"].keys())}
 rel_type_dict.pop("None")
+
+user_tokens = ["[UNU]"]+[f"[USER{i}]" for i in range(data_config["max_users"])]
+num_mask_pos = 3
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class precision_recall_fscore():
 
@@ -51,11 +56,8 @@ class precision_recall_fscore():
         self.refs = []
 
         return f1_metrics
-		   
-        
-metric = precision_recall_fscore()
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+metric = precision_recall_fscore()
 
 def get_tok_model(tokenizer_version, model_version):
     tokenizer = LongformerTokenizer.from_pretrained(tokenizer_version)
@@ -65,7 +67,6 @@ def get_tok_model(tokenizer_version, model_version):
     if model_version=='allenai/longformer-base-4096':
         transformer_model.resize_token_embeddings(len(tokenizer))
     return tokenizer, transformer_model
-
 
 """#### Load in discourse markers"""
 
@@ -85,13 +86,10 @@ def get_datasets(train_sz=100, test_sz=0):
 """### Define linear layer for a relation type prediction"""
 
 def get_rel_head():
-    linear_layer = nn.Linear(2*transformer_model.config.hidden_size,
+    linear_layer = nn.Linear(num_mask_pos*transformer_model.config.hidden_size,
                              len(rel_type_dict)).to(device)
 
     return linear_layer
-
-cross_entropy_layer = nn.CrossEntropyLoss(weight=torch.tensor([0.508, 2.027, 2.402, 2.234, 2.667], 
-                                                              device=device), reduction='sum')
 
 """### Global Attention Mask Utility for Longformer"""
 
@@ -100,151 +98,169 @@ def get_global_attention_mask(tokenized_threads: np.ndarray) -> np.ndarray:
     0 elsewhere.
     """
     mask = np.zeros_like(tokenized_threads)
-    for user_token in ["[UNU]"]+[f"[USER{i}]" for i in range(data_config["max_users"])]:
+    for user_token in user_tokens:
         user_token_id = tokenizer.encode(user_token)[1:-1]
         mask = np.where(tokenized_threads==user_token_id, 1, mask)
     return np.array(mask, dtype=bool)
 
-
-"""### Loss and Prediction Function"""
-def get_comp_wise_means(logits: torch.Tensor, 
-                        comp_type_label: torch.Tensor,
-                        tokenized_thread: torch.Tensor) -> torch.Tensor:
-    """Averages logits, component wise.
-    Args:
-        logits:           logits of shape [seq_len, hidden_dim]
-        
-        comp_type_label:  argumentative component type of each token whose
-                          logits are provided. Shape: [seq_len]
-        
-        tokenized_thread: the tokenized thread whose logits are provided.
-                          Shape: [seq_len]
-    Returns:
-        An array of size [n_comps, hidden_dim] where n_comps is the number of 
-        components in the comp_type_label. 
-    """
-    comp_means = []
-    length = torch.sum(tokenized_thread!=tokenizer.pad_token_id)
+def get_spans(comp_type_labels, length):
     
     def detect_span(start_idx: int, span_t: str):
         j = start_idx
-        while comp_type_label[j]==ac_dict[span_t] and j<length:
+        while comp_type_labels[j]==ac_dict[span_t] and j<length:
             j += 1
         end_idx = j
         return start_idx, end_idx
     
     i=0
+    spans_lis = []
     while i<length:
-        if comp_type_label[i]==ac_dict["O"]:
-            start_idx, end_idx = detect_span(i, "O")
+        if comp_type_labels[i]==ac_dict["O"]:
+            _start_idx, end_idx = detect_span(i, "O")
+
+        elif comp_type_labels[i]==ac_dict["B-C"]:
+            _start_idx, end_idx = detect_span(i+1, "I-C")
+            spans_lis.append((i, end_idx))
+            
+        elif comp_type_labels[i]==ac_dict["B-P"]:
+            _start_idx, end_idx = detect_span(i+1, "I-P")
+            spans_lis.append((i, end_idx))
         
-        elif comp_type_label[i]==ac_dict["B-C"]:
-            start_idx, end_idx = detect_span(i+1, "I-C")
-            comp_means.append(torch.mean(logits[start_idx-1:end_idx], dim=0))
-        
-        elif comp_type_label[i]==ac_dict["B-P"]:
-            start_idx, end_idx = detect_span(i+1, "I-P")
-            comp_means.append(torch.mean(logits[start_idx-1:end_idx], dim=0))
-        
-        elif (comp_type_label[i]==ac_dict["I-C"] or 
-              comp_type_label[i]==ac_dict["I-P"]):
+        elif (comp_type_labels[i]==ac_dict["I-C"] or
+              comp_type_labels[i]==ac_dict["I-P"]):
             raise AssertionError("Span detection not working properly, \
                                   Or intermediate tokens without begin tokens in",
-                                 comp_type_label)
+                                 comp_type_labels)
         else:
-            raise ValueError("Unknown component type:", comp_type_label[i], 
+            raise ValueError("Unknown component type:", comp_type_labels[i], 
                              "Known types are:", ac_dict)
         
         i = end_idx
     
-    return torch.stack(comp_means)
+    return spans_lis
 
-def relation_type_pred(comp_encodings: torch.Tensor, 
-                       refers_to_and_type: np.ndarray) -> torch.Tensor:
-    """
-    Args:
-        comp_encodings:     The encodings for various components in a 
-                            tokenized_thread.
-        refers_to_and_type: An array having entries of the form [i,j,k], where,
-                            the entry denotes link from component i to component
-                            j of type k. Component nos. are 1-indexed. Link to 0
-                            indicates that component isn't linked to any other one.
-    Returns:
-        logits corresponding to relation types for relations(excluding the 
-        relations to 0) in refers_to_and_type. The logits occur in the same order
-        as the relations' orders in refers_to_and_type.
-    """
-    logits = []
+def get_user_token_positions(tokenized_thread):
+    user_token_indices = [tokenizer.encode(user_token)[1] for user_token in user_tokens]
+    user_token_pos = []
+    for i, elem in enumerate(tokenized_thread):
+        if elem in user_token_indices:
+            user_token_pos.append(i)
+    return user_token_pos
 
-    for (from_comp, to_comp, _rel_type) in refers_to_and_type:
-        if to_comp==0:
-            continue
-            
-        logits.append(linear_layer(torch.cat([comp_encodings[from_comp-1], 
-                                              comp_encodings[to_comp-1]])))
+def generate_prompts(tokenized_thread, comp_type_labels, refers_to_and_type):
+
+    length = np.sum(tokenized_thread!=tokenizer.pad_token_id)
+    spans_lis = get_spans(comp_type_labels, length)
+    print("First two spans:", spans_lis[0], spans_lis[1])
+    user_token_pos = get_user_token_positions(tokenized_thread)
+    for (link_from, link_to, rel_type) in refers_to_and_type:
         
-    return torch.stack(logits)
-
-def get_rel_types(refers_to_and_type: np.ndarray) -> torch.Tensor:
-    """
-    Args:
-        refers_to_and_type:   Same as in relation_type_pred
-    Returns:
-        A pytorch tensor of type long having labels of various existant 
-        relations.
-    """
-    label_rel_types = []
-    for (_from_comp, to_comp, rel_type) in refers_to_and_type:
-        if to_comp==0:
+        if link_to==0:
             continue
-        label_rel_types.append(rel_type)
-    return torch.tensor(label_rel_types, dtype=torch.long, device=device)
+        
+        from_start_idx, from_end_idx = spans_lis[link_from-1]
+        to_start_idx, to_end_idx = spans_lis[link_to-1]
+        
+        print("Link from:", tokenizer.decode(tokenized_thread[from_start_idx:from_end_idx]), from_start_idx, from_end_idx)
+        print("Link to:", tokenizer.decode(tokenized_thread[to_start_idx:to_end_idx]), to_start_idx, to_end_idx)
+        
+        from_user_token_id, to_user_token_id = 0, 0
+        
+        for elem in user_token_pos:
+            if elem<=from_start_idx:
+                from_user_token_id = tokenized_thread[elem]
+            if elem<=to_start_idx:
+                to_user_token_id = tokenized_thread[elem]
+            if elem>from_start_idx and elem>to_start_idx:
+                break
+        
+        from_seq_length = from_end_idx-from_start_idx
+        to_seq_length = to_end_idx - to_start_idx
+        
+        prompt = np.copy(tokenized_thread)
+        prompt[length] = to_user_token_id
+        prompt[length+1] = tokenizer.encode(" said")[1]
+        
+        mask_pos = length+2+to_seq_length
+        
+        prompt[length+2:mask_pos] = tokenized_thread[to_start_idx:to_end_idx]
+        prompt[mask_pos:mask_pos+num_mask_pos] = [tokenizer.mask_token_id]*num_mask_pos
+        
+        prompt[mask_pos+num_mask_pos] = from_user_token_id
+        prompt[mask_pos+num_mask_pos+1] = tokenizer.encode(" said")[1]
+        prompt[mask_pos+num_mask_pos+2:mask_pos+num_mask_pos+2+from_seq_length] = tokenized_thread[from_start_idx:from_end_idx]
+        
+        yield (prompt, rel_type)
+
+def get_prompt_generator(dataset, batch_size, shuffle=True):
+    prompt_dataset = []
+    
+    for (tokenized_threads, _, comp_type_labels, refers_to_and_type) in dataset:
+        tokenized_threads = tokenized_threads[0]
+        comp_type_labels = comp_type_labels[0]
+        refers_to_and_type = refers_to_and_type[0]
+        for (sample_tokenized_thread, 
+            sample_comp_type_labels, 
+            sample_refers_to_and_type) in zip(tokenized_threads, 
+                                            comp_type_labels, 
+                                            refers_to_and_type):
+                
+            prompt_dataset += [elem for elem in generate_prompts(sample_tokenized_thread, 
+                                                                sample_comp_type_labels,
+                                                                sample_refers_to_and_type)]
+    if shuffle:
+        random.shuffle(prompt_dataset)
+    
+    def prompt_dataset_gen():
+        batch_of_prompts = []
+        rel_type_labels = []
+        for prompt, rel_type in prompt_dataset:
+            batch_of_prompts.append(prompt)
+            rel_type_labels.append(rel_type)
+            if len(batch_of_prompts)==batch_size:
+                yield batch_of_prompts, rel_type_labels
+                batch_of_prompts, rel_type_labels = [], []
+    
+    return prompt_dataset_gen
+
+"""### Loss and Prediction Function"""
+
+cross_entropy_layer = nn.CrossEntropyLoss(weight=torch.tensor([0.508, 2.027, 2.402, 2.234, 2.667], 
+                                                              device=device), reduction='sum')
 
 def compute(batch: Tuple[torch.Tensor, torch.Tensor, torch.Tensor],
             preds: bool=False,) -> Union[List[torch.Tensor], torch.Tensor]:
     """
     Args:
-        batch:  A tuple having tokenized thread of shape [batch_size, seq_len],
-                component type labels of shape [batch_size, seq_len], and a global
-                attention mask for Longformer, of the same shape.
+        batch:  A tuple having prompting thread of shape [batch_size, seq_len],
+                and relation type labels of shape [batch_size], and global attention mask
+                for longformer of shape [batch_size, seq_len] with 1 for tokens which attend
+                globally.
         
-        preds:  If True, returns a List(of batch_size size) of Tuples of form 
-                (tag_sequence, viterbi_score) where the tag_sequence is the 
-                viterbi-decoded sequence, for the corresponding sample in the batch.
-        
-        cross_entropy:  This argument will only be used if preds=False, i.e., if 
-                        loss is being calculated. If True, then cross entropy loss
-                        will also be added to the output loss.
-    
+        preds:  If True, returns a List(of batch_size size) of relation predictions
+                for correspoding prompts in the batch.
     Returns:
         Either the predicted relations(if preds is True), or the loss value 
         summed over all samples of the batch (if preds is False).
     """
-    tokenized_threads, comp_type_labels, refers_to_and_type, global_attention_mask = batch
+    prompt_threads, rel_type_labels, global_attention_mask = batch
     
-    pad_mask = torch.where(tokenized_threads!=tokenizer.pad_token_id, 1, 0)
+    pad_mask = torch.where(prompt_threads!=tokenizer.pad_token_id, 1, 0)
     
-    logits = transformer_model(input_ids=tokenized_threads,
-                               attention_mask=pad_mask,
-                               global_attention_mask=global_attention_mask).last_hidden_state
+    hidden_state = transformer_model(input_ids=prompt_threads,
+                                     attention_mask=pad_mask,
+                                     global_attention_mask=global_attention_mask).last_hidden_state
     
-    comp_wise_logits = [get_comp_wise_means(sample_logits, sample_comp_type_labels, sample_tokenized_thread)
-                            for (sample_logits, sample_comp_type_labels, sample_tokenized_thread)
-                                 in zip(logits, comp_type_labels, tokenized_threads)]
-    
-    rel_type_logits =  [relation_type_pred(elem1, elem2) 
-                        for elem1, elem2 in zip(comp_wise_logits, 
-                                                refers_to_and_type)]
-    
-    
-    if preds:
-        return [torch.max(elem, dim=-1).indices for elem in rel_type_logits]
-    
-    rel_labels = [get_rel_types(elem) for elem in refers_to_and_type]
+    hidden_state = hidden_state[prompt_threads==tokenizer.mask_token_id]
+    hidden_state = hidden_state.reshape(-1, num_mask_pos, hidden_state.shape[-1])
+    hidden_state = torch.flatten(hidden_state, start_dim=1)
+    rel_type_logits = linear_layer(hidden_state)
 
-    ce_loss = sum([cross_entropy_layer(elem1, elem2)
-                   for elem1, elem2 in zip(rel_type_logits, rel_labels)])
+    if preds:
+        return torch.max(rel_type_logits, dim=-1).indices
     
+    ce_loss = cross_entropy_layer(rel_type_logits, rel_type_labels)
+
     return ce_loss
 
 """### Training And Evaluation Loops"""
@@ -254,22 +270,20 @@ def train(dataset):
     
     optimizer.zero_grad()
 
-    for i, (tokenized_threads, _, comp_type_labels, refers_to_and_type) in enumerate(dataset):
-        global_attention_mask = torch.tensor(get_global_attention_mask(tokenized_threads),
+    for i, (prompt_threads, rel_type_labels) in enumerate(
+                                                    get_prompt_generator(dataset, 
+                                                                         batch_size=data_config["batch_size"])):
+        
+        global_attention_mask = torch.tensor(get_global_attention_mask(prompt_threads),
                                              device=device, dtype=torch.int32)
         
-        #Remove Device Axis and cast to PyTorch tensor
-        tokenized_threads = torch.tensor(np.squeeze(tokenized_threads, axis=0), 
-                                         device=device)
-        comp_type_labels = torch.tensor(np.squeeze(comp_type_labels, axis=0), 
-                                         device=device)
-        refers_to_and_type = np.squeeze(refers_to_and_type, axis=0)
-
+        #Cast to PyTorch tensor
+        prompt_threads = torch.tensor(prompt_threads, device=device)
+        rel_type_labels = torch.tensor(rel_type_labels, device=device)
         global_attention_mask = torch.squeeze(global_attention_mask, dim=0)
         
-        loss = compute((tokenized_threads,
-                        comp_type_labels,
-                        refers_to_and_type,
+        loss = compute((prompt_threads,
+                        rel_type_labels,
                         global_attention_mask))/data_config["batch_size"]
 
         print("Loss:", loss)
@@ -287,33 +301,27 @@ def evaluate(dataset, metric):
     int_to_labels = {v:k for k, v in rel_type_dict.items()}
 
     with torch.no_grad():
-        for tokenized_threads, _, comp_type_labels, refers_to_and_type in dataset:
+        for prompt_threads, rel_type_labels in get_prompt_generator(dataset,
+                                                                    batch_size=data_config["batch_size"]):
             print("Evaluating") 
             global_attention_mask = torch.tensor(get_global_attention_mask(tokenized_threads), 
                                                  device=device)
             
-            #Remove Device Axis and cast to PyTorch tensor
-            tokenized_threads = torch.tensor(np.squeeze(tokenized_threads, axis=0),
-                                            device=device)
-            comp_type_labels = torch.tensor(np.squeeze(comp_type_labels, axis=0),
-                                            device=device)
-            refers_to_and_type = np.squeeze(refers_to_and_type)
-
+            #Cast to PyTorch tensor
+            prompt_threads = torch.tensor(prompt_threads, device=device)
+            rel_type_labels = torch.tensor(rel_type_labels, device=device)
             global_attention_mask = torch.squeeze(global_attention_mask, dim=0)
             
-            preds = compute((tokenized_threads,
-                             comp_type_labels,
-                             refers_to_and_type,
+            preds = compute((prompt_threads,
+                             rel_type_labels,
                              global_attention_mask),
                             preds=True)
             
-            preds = [[int_to_labels[pred.item()] for pred in seq] for seq in preds]
-            refs =  [[int_to_labels[ref.item()] for ref in get_rel_types(elem)] 
-                     for elem in refers_to_and_type]
+            preds = [int_to_labels[pred.item()] for pred in preds]
+            refs =  [int_to_labels[ref.item()] for ref in rel_type_labels]
             
-            for elem1, elem2 in zip(preds, refs):
-                metric.add_batch(predictions=elem1,
-                                 references=elem2,)
+            metric.add_batch(predictions=preds,
+                             references=refs,)
         
     print("\t\t\t\t", metric.compute())
 
@@ -337,7 +345,7 @@ for (tokenizer_version, model_version) in [('../home/arg_mining/4epoch_complete/
             print(f"\n\n\t\t-------------RUN {run+1}-----------")
             
             tokenizer, transformer_model = get_tok_model(tokenizer_version, model_version)
-            
+
             linear_layer = get_rel_head()
             
             optimizer = optim.Adam(params = chain(transformer_model.parameters(),
