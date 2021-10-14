110b2f3f11635fe78e3609bcfda4103b65f2d155
Jeevesh8 <creativityinczenyoga@gmail.com>
2021-10-14 22:48:51 +0530

    Added logs; scripts for generating scores

---
+++ utils/get_classwise_prf_scores.sh
@@ -0,0 +1,81 @@
+#REGEX="'precision':.*?'parts_of_same': 0.(\d+)"
+#echo $REGEX
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_base_lf --names "base Longformer prompting 80-20" --regexp "$REGEX" --split 1
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_ckpt4 --names "sMLM ckpt4 prompting 80-20" --regexp "$REGEX" --split 1
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_base_lf --names "base Longformer prompting 50-50" --regexp "$REGEX" --split 2
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_ckpt4 --names "sMLM ckpt4 prompting 50-50" --regexp "$REGEX" --split 2
+
+#REGEX="'recall':.*?'parts_of_same': 0.(\d+)"
+#echo $REGEX
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_base_lf --names "base Longformer prompting 80-20" --regexp "$REGEX" --split 1
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_ckpt4 --names "sMLM ckpt4 prompting 80-20" --regexp "$REGEX" --split 1
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_base_lf --names "base Longformer prompting 50-50" --regexp "$REGEX" --split 2
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_ckpt4 --names "sMLM ckpt4 prompting 50-50" --regexp "$REGEX" --split 2
+
+#REGEX="'f1':.*?'parts_of_same': 0.(\d+)"
+#echo $REGEX
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_base_lf --names "base Longformer prompting 80-20" --regexp "$REGEX" --split 1
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_ckpt4 --names "sMLM ckpt4 prompting 80-20" --regexp "$REGEX" --split 1
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_base_lf --names "base Longformer prompting 50-50" --regexp "$REGEX" --split 2
+#python3 last_n_means.py --in_files ../logs/DrInv_out_prompt_rel_pred_ckpt4 --names "sMLM ckpt4 prompting 50-50" --regexp "$REGEX" --split 2
+
+REGEX="'precision':.*?'partial': 0.(\d+)"
+echo $REGEX
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_base_lf --names "base Longformer prompting 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_ckpt_4 --names "sMLM ckpt4 prompting 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_base_lf --names "base Longformer prompting 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_ckpt_4 --names "sMLM ckpt4 prompting 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_base_lf --names "base Longformer mean pooling 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_ckpt4 --names "sMLM ckpt4 mean pooling 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_base_lf --names "base Longformer mean pooling 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_ckpt4 --names "sMLM ckpt4 mean pooling 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_roberta_CmvModes --names "Contextless Roberta 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_QRbert_CmvModes --names "Contextless QR-Bert 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_roberta_CmvModes --names "Contextless Roberta 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_QRbert_CmvModes --names "Contextless QR-Bert 50-50" --regexp "$REGEX" --split 2
+
+
+REGEX="'recall':.*?'partial': 0.(\d+)"
+echo $REGEX
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_base_lf --names "base Longformer prompting 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_ckpt_4 --names "sMLM ckpt4 prompting 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_base_lf --names "base Longformer prompting 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_ckpt_4 --names "sMLM ckpt4 prompting 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_base_lf --names "base Longformer mean pooling 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_ckpt4 --names "sMLM ckpt4 mean pooling 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_base_lf --names "base Longformer mean pooling 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_ckpt4 --names "sMLM ckpt4 mean pooling 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_roberta_CmvModes --names "Contextless Roberta 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_QRbert_CmvModes --names "Contextless QR-Bert 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_roberta_CmvModes --names "Contextless Roberta 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_QRbert_CmvModes --names "Contextless QR-Bert 50-50" --regexp "$REGEX" --split 2
+
+
+REGEX="'f1':.*?'partial': 0.(\d+)"
+echo $REGEX
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_base_lf --names "base Longformer prompting 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_ckpt_4 --names "sMLM ckpt4 prompting 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_base_lf --names "base Longformer prompting 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_prompt_rel_pred_ckpt_4 --names "sMLM ckpt4 prompting 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_base_lf --names "base Longformer mean pooling 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_ckpt4 --names "sMLM ckpt4 mean pooling 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_base_lf --names "base Longformer mean pooling 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_5cons_runs_ckpt4 --names "sMLM ckpt4 mean pooling 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_roberta_CmvModes --names "Contextless Roberta 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_QRbert_CmvModes --names "Contextless QR-Bert 80-20" --regexp "$REGEX" --split 1
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_roberta_CmvModes --names "Contextless Roberta 50-50" --regexp "$REGEX" --split 2
+python3 last_n_means.py --in_files ../logs/out_rel_pred_contextless_QRbert_CmvModes --names "Contextless QR-Bert 50-50" --regexp "$REGEX" --split 2
+
+
+#REGEX="'D':.*?'f1': 0.(\d+)"
+#echo $REGEX
+#python3 last_n_means.py --in_files ../logs/out_5cons_runs_base_lf_drInventor --names "base Longformer" --regexp "$REGEX" --split 1
+#python3 last_n_means.py --in_files ../logs/out_5cons_runs_ckpt_4_drInventor --names "sMLM ckpt4" --regexp "$REGEX" --split 1
+
+#python3 last_n_means.py --in_files ../logs/out_5cons_runs_cw_bert_base_cased --names "bert-base-cased" --regexp "$REGEX" --split 2
+#python3 last_n_means.py --in_files ../logs/out_5cons_runs_cw_bert_ckpt4 --names "bert sMLM ckpt-4" --regexp "$REGEX" --split 2
+#python3 last_n_means.py --in_files ../logs/out_5cons_runs_cw_base_roberta --names "Roberta" --regexp "$REGEX" --split 2
+#python3 last_n_means.py --in_files ../logs/out_cw_5cons_runs_base_lf --names "Base LF" --regexp "$REGEX" --split 2
+#python3 last_n_means.py --in_files ../logs/out_5cons_runs_base_lf_CmvModes_Global  --names "Base LF" --regexp "$REGEX" --split 2
+#python3 last_n_means.py --in_files ../logs/out_5cons_runs_base_lf_256attWindow  --names "Base LF-256 attention window" --regexp "$REGEX" --split 2
+#python3 last_n_means.py --in_files ../logs/out_5cons_runs_ckpt_4_CmvModes  --names "LF-ckpt4" --regexp "$REGEX" --split 2
---
+++ utils/last_n_means.py
@@ -0,0 +1,81 @@
+import argparse
+import re, math
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+def get_runwise_data(output_file):
+    """Get data in form of list of scores, and their standard deviations 
+    from an output file of a run."""
+    with open(output_file) as f:
+        data = f.read().split('Train size')[args.split]
+                    
+    run_epoch_wise_data = []
+    for _run_no, run_data in enumerate(data.split('RUN')[1:]):
+        run_epoch_wise_data.append([])
+        for epoch_no, epoch_data in enumerate(run_data.split('EPOCH')[1:]):
+            
+            try:
+                regex = re.compile(args.regexp)
+                f1 = re.findall(regex, epoch_data)[0]
+                print(f1)
+                f1 = int(f1)/(10**len(f1))
+                run_epoch_wise_data[-1].append(f1)
+            
+            except IndexError:
+                if epoch_no!=len(run_data.split('EPOCH')[1:])-1:
+                    run_epoch_wise_data[-1].append(0.0)
+                else:
+                    print("Error: Could not find any score in output file, for the provided regular expression")
+                    exit(1)
+                
+    for elem in run_epoch_wise_data:
+        print("Read data for:", len(elem), "epochs.")
+    
+    n_epochs = min([len(elem) for elem in run_epoch_wise_data])
+    print("Finally considering data upto:", n_epochs, "epochs.")
+
+    return [[run_wise_data[i] for run_wise_data in run_epoch_wise_data]
+            for i in range(n_epochs)]
+
+def mean(lis):
+    return sum(lis)/len(lis)
+
+def bessel_corrected_std(lis):
+    m = mean(lis)
+    variances = [(elem-m)*(elem-m) for elem in lis]
+    return 0 if len(lis)<=1 else math.sqrt(sum(variances)/(len(lis)-1))
+
+def get_mean_and_error(epoch_run_wise_data):
+    means = []
+    stds = []
+    for epoch_data in epoch_run_wise_data:
+        means.append(mean(epoch_data))
+        stds.append(bessel_corrected_std(epoch_data))
+    return means, stds
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="Plots the apt score for all the input files onto a single graph.")
+    parser.add_argument("--in_files", type=str, nargs="+", required=True, help="Input files containing data to plot.")
+    parser.add_argument("--names", type=str, nargs="+", required=True, help="Names for various plots of various files.")
+    parser.add_argument("--avg_over", type=int, default=5, help = "The mean scores printed are averaged over this many last scores.")
+    parser.add_argument("--regexp", type=str, required=True, help="Regular expression to search for scores. ")
+    parser.add_argument("--split", type=int, default=1, help="Index of split to choose, after splitting the data in a file on \"Train size\"")
+    args = parser.parse_args()
+
+    means = []
+    stds = []
+    for filename in args.in_files:
+        mean_lis, std_lis = get_mean_and_error(get_runwise_data(filename))
+        means.append(mean_lis)
+        stds.append(std_lis)
+    
+    if args.avg_over>=len(means[0]):
+        raise ValueError("Not enough epochs to average over. Kindly decrease the epochs to \
+                          average over via --avg_over. No. of epochs:", len(means), 
+                          "Averaging over:", args.avg_over)
+    
+    print("Mean scores for files provided, over the last", args.avg_over,"runs, in order:")
+    
+    for mean_lis, name in zip(means, args.names):
+        print("\t", "Score for", name, ":", mean(mean_lis[-args.avg_over:]))
