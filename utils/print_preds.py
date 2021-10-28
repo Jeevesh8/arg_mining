d9c3464e96498a3b38a34b432399e476dc87b5c8
Jeevesh8 <creativityinczenyoga@gmail.com>
2021-10-28 21:05:42 +0530

    debug print_preds.py

--- utils/print_preds.py
+++ utils/print_preds.py
@@ -1,7 +1,3 @@
-from arg_mining.datasets.cmv_modes import data_config
-
-ac_dict = data_config["arg_components"]
-
 def print_preds(tokenized_threads, tokenizer, preds, refs):
     for i, (thread, pred, ref) in enumerate(zip(tokenized_threads, preds, refs)):
         print("Thread: ", thread)
@@ -10,39 +6,39 @@ def print_preds(tokenized_threads, tokenizer, preds, refs):
         print("Thread Text:", tokenizer.decode(thread))
         print("Predicted Components:")
         i=0
-        while i<=thread.shape[0] and thread[i]!=tokenizer.pad_token_id:
-            if pred[i]==ac_dict["B-C"]:
+        while i<thread.shape[0] and thread[i]!=tokenizer.pad_token_id:
+            if pred[i]=="B-C":
                 start_idx = i
                 i += 1
-                while pred[i]==ac_dict["I-C"]:
+                while i<len(pred) and pred[i]=="I-C":
                     i += 1
                 end_idx = i
-                print("\t","Claim:",tokenizer.decode(thread[start_idx:end_idx]))
-            elif pred[i]==ac_dict["B-P"]:
+                print("\tClaim:",tokenizer.decode(thread[start_idx:end_idx]))
+            elif pred[i]=="B-P":
                 start_idx = i
-                while pred[i]==ac_dict["I-P"]:
+                while i< len(pred) and pred[i]=="I-P":
                     i += 1
                 end_idx = i
-                print("\t", "Premise:", tokenizer.decode(thread[start_idx:end_idx]))
+                print("\tPremise:", tokenizer.decode(thread[start_idx:end_idx]))
             else:
                 i += 1
         
         print("Actual Components:")
         i=0
-        while i<=thread.shape[0] and thread[i]!=tokenizer.pad_token_id:
-            if ref[i]==ac_dict["B-C"]:
+        while i<thread.shape[0] and thread[i]!=tokenizer.pad_token_id:
+            if ref[i]=="B-C":
                 start_idx = i
                 i += 1
-                while ref[i]==ac_dict["I-C"]:
+                while i<len(ref) and ref[i]=="I-C":
                     i += 1
                 end_idx = i
-                print("\t", "Claim:", tokenizer.decode(thread[start_idx:end_idx]))
-            elif ref[i]==ac_dict["B-P"]:
+                print("\tClaim:", tokenizer.decode(thread[start_idx:end_idx]))
+            elif ref[i]=="B-P":
                 start_idx = i
-                while pred[i]==ac_dict["I-P"]:
+                while i<len(ref) and ref[i]=="I-P":
                     i += 1
                 end_idx = i
-                print("\t", "Premise:", tokenizer.decode(thread[start_idx:end_idx]))
+                print("\tPremise:", tokenizer.decode(thread[start_idx:end_idx]))
             else:
                 i += 1
             
